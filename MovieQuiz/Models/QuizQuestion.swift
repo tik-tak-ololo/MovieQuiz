1a43c5e949c6ab0f98a375d6504f32420f8e21ca
tik-tak-ololo <tik-tak-ololo@outlook.com>
2025-12-25 02:36:09 +0700

    sprint 6 completed

--- MovieQuiz/Models/QuizQuestion.swift
+++ MovieQuiz/Models/QuizQuestion.swift
@@ -8,9 +8,9 @@
 import Foundation
 
 struct QuizQuestion {
-    // строка с названием фильма,
-    // совпадает с названием картинки афиши фильма в Assets
-    let image: String
+
+    // картинка
+    let image: Data
     
     // строка с вопросом о рейтинге фильма
     let text: String
