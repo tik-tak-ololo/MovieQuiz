bac4b2e2879b73a09ab01c701db5e5b61e9a22de
tik-tak-ololo <tik-tak-ololo@outlook.com>
2026-01-02 14:21:30 +0700

    The function of generating data for questions has been implemented.

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -172,7 +172,7 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate {
                     
                     self.currentQuestionIndex = 0
                     self.correctAnswers = 0
-                    self.questionFactory?.requestNextQuestion()
+                    self.questionFactory?.loadData()
                     
                 }
         
