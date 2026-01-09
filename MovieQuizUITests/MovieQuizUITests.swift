8cd21afd001ab8ba0ae268a7926de465c74c500f
tik-tak-ololo <tik-tak-ololo@outlook.com>
2026-01-10 02:38:41 +0700

    style

--- MovieQuizUITests/MovieQuizUITests.swift
+++ MovieQuizUITests/MovieQuizUITests.swift
@@ -29,23 +29,6 @@ final class MovieQuizUITests: XCTestCase {
         app.terminate()
         app = nil
     }
-
-//    @MainActor
-//    func testExample() throws {
-//        // UI tests must launch the application that they test.
-//        let app = XCUIApplication()
-//        app.launch()
-//
-//        // Use XCTAssert and related functions to verify your tests produce the correct results.
-//    }
-//
-//    @MainActor
-//    func testLaunchPerformance() throws {
-//        // This measures how long it takes to launch your application.
-//        measure(metrics: [XCTApplicationLaunchMetric()]) {
-//            XCUIApplication().launch()
-//        }
-//    }
     
     func testYesButton() {
         
